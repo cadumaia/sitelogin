39735f3cccc61bf2a769f96fb530ae332827365d
Carlos Eduardo Maia <cadumaia12@gmail.com>
2023-05-30 16:40:17 -0300

    .

---
+++ SiteDaLoja-main/LayoutDoSite/src/Perfil.js
@@ -0,0 +1,106 @@
+import { getAuth, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "./firebase";
+import{
+  doc,
+  updateDoc,
+} from "firebase/firestore"
+
+export default function Perfil() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+    toast.success("Você deslogou")
+  }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update name in the firestore
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Perfil Atualizado!");
+    } catch (error) {
+      toast.error("Não foi possível atualizar seu perfil");
+    }
+  }
+
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">Meu Perfil</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/* Name Input */}
+
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
+
+            {/* Email Input */}
+
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center ">
+                Deseja mudar seu nome de usuário?
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Logout
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ SiteDaLoja-main/LayoutDoSite/src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Cadastro from './Cadastro';
+import Carrinho from './Carrinho';
+import Login from './Login'
+import FormasPag from './FormasPag';
+import Produto1 from './Produto1';
+import Perfil from './Perfil';
+import ForgotPassword from './ForgotPassword';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/Cadastro",
+    element: <Cadastro />
+  },
+  {
+    path: "/Carrinho",
+    element: <Carrinho />
+  },
+  {
+    path: "/Login",
+    element: <Login />
+  },
+  {
+    path: "/FormasPag",
+    element: <FormasPag />
+  },
+  {
+    path: "/Produto1",
+    element: <Produto1 />
+  },
+  {
+    path: "/Perfil",
+    element: <Perfil />
+  },
+  {
+    path: "/ForgotPassword",
+    element: <ForgotPassword />
+  }
+])
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
